e1c2ed685fa04a14935795598158cfdbe4455b85
David Manor <david@dolphextechnologies.com>
2021-04-23 03:08:06 +0100

    [add] fetch single plant  w/ id route

--- routers/plant.js
+++ routers/plant.js
@@ -21,3 +21,15 @@ router.post('/plants', auth, async (req, res)=>{
         res.status(400).send()
     }
   })
+
+  //get single plant with id
+router.get("/plants/:id", async (req, res) => {
+    const plantID = req.params.id;
+  
+    try {
+      const plant = await Plant.findById(plantID).exec();
+      res.status(200).send(plant);
+    } catch (error) {
+      res.status(404).send();
+    }
+  });
